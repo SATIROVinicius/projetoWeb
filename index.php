9160c7dd77afbb1fa13e916be6352f415a719b20
Vinícius Sátiro <viniciuscomnet2@gmail.com>
2019-09-01 19:39:13 -0300

    primeiro

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang=“pt-br”>
+	<head>
+		<title>Itaipú Veículos</title>
+		<meta charset=“utf-8”/>
+		<link rel="stylesheet" href="estilos.css"/> 
+	</head>
+	<body>
+	
+		<header>
+			<?php
+				include "topo.php";
+			?>
+		</header>
+		<section >
+			<?php
+				include "slider.html";
+			?>
+		</section>
+		
+	</body>
+</html> 
